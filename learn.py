0e3334c6cdffe34497724dbbc8519f5863418f90
Courtskit <Courtskit@gmail.com>
2020-07-10 11:51:28 -0700

    py file set up

---
+++ learn.py
@@ -0,0 +1,11 @@
+print("Hello World")
+
+name = input("What is your name? ")
+
+print("It's nice to meet you,", name)
+answer = input("Do you like to code? ")
+
+if answer == "yes":
+  print("That's awesome to hear, me too!")
+else:
+  print("Oh no! That makes me sad.")
